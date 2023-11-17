95b498f817fdcd9d035d3abc834c2e41d5e36811
matina maharjan <146967495+matinasawr@users.noreply.github.com>
2023-11-17 10:27:45 -0500

    Add files via upload

---
+++ convertor.py
@@ -0,0 +1,41 @@
+import sys, re
+
+def replace_keep_case(word, replacement, text): 
+	def func(match):
+		g = match.group ()
+		#print ('!', match, "|", g, file=sys.stderr)
+		if g.islower(): return replacement.lower() 
+		if g.istitle(): return replacement.title() 
+		if g.isupper(): return replacement.upper()
+		return replacement
+	#return re.sub(word, func, text)#, flags=re.I)
+	return re.sub(word, replacement, text)#, flags=re.I)
+
+line = sys.stdin.readline().strip()
+
+table = {}
+for line in open('newariIPA.csv').readlines():
+	row = line.split(',')
+	nw = row[0].strip().upper()
+	IPA = row[1].strip()
+	table[nw]=IPA 
+	
+while line:
+
+	tokens = re.sub('([:,.!?]+)', ' \g<1> ', line.upper()).split (' ')
+	normalized_tokens = []
+	
+	for token in tokens:
+		for nw,IPA in table.items():
+			token = replace_keep_case(nw,IPA,token)
+		normalized_tokens.append(token.strip())
+
+	newline = ' '.join(normalized_tokens)
+	print(re.sub('  *', ' ', newline))
+
+	line = sys.stdin.readline().strip()
+
+	# kala_sentence = 'kala is the IPA for machine'
+	# re.sub('kala', 'kələ', kala_sentence)
+	# print(kala_sentence)
+
