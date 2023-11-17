555ba0f7a8d6388b670fc9ac99df46bc174d9986
matina maharjan <146967495+matinasawr@users.noreply.github.com>
2023-11-17 10:36:33 -0500

    / python

---
+++ process-wiktionary.py
@@ -0,0 +1,58 @@
+import sys, re
+
+line = sys.stdin.readline() # Read in a line
+inside = False # We are not in the Newari part of the page
+ipa = '_' # A variable to hold the current IPA 
+newari_form = '_'
+devanagari_form = '_'
+transcription = '_'
+pos = '_'
+translation = '_'
+finished = False
+
+while line:
+	line = line.strip('\n')
+	if line.count('Newar') > 0 and line.count('[edit]') > 0: 
+		# We saw the Newari heading
+		inside = True
+	if line.count('Retrieved from') > 0:
+		# We are now outside Newari part
+		inside = False
+		finished = True
+	if re.findall('(Rajasthani|Sanskrit)', line):
+		inside = False
+		finished = True
+
+	if line.count('IPA(key)') > 0:
+		# We found a line with IPA, so split
+		# it by : and take the second half
+		ipa = line.split(':')[1].strip()
+
+	for pos_ in ['Noun', 'Verb', 'Adjective', 'Adverb', 'Pronoun']: 
+		if pos_ + '[edit]' in line:
+			pos = pos_
+
+	if line.count('•') > 0:
+		# 𑐎𑐮 • (kala) ? (Deva spelling कल)		
+		transcription = re.findall('\([^)]+\)', line)[0]
+		if line.lower().count('deva spelling') > 0:
+			newari_form = line.split('•')[0].strip()
+			devanagari_form = line.lower().split('deva spelling')[1].strip(')')
+		elif line.lower().count('newa spelling') > 0:
+			devanagari_form = line.split('•')[0].strip()
+			newari_form = line.lower().split('newa spelling')[1].strip(')')
+		# else:
+		# 	print('!', line)
+
+	if re.findall('[0-9]+\.', line):
+		translation = line.strip()
+	
+	# Print out the line, IPA and the state (inside or outside)
+
+	if finished:
+		if newari_form != '_' and devanagari_form != '_' :
+			print(f'{newari_form} | {devanagari_form} | {ipa} | {transcription} | {pos} | {translation}')
+		break
+
+	line = sys.stdin.readline() # Read in next line
+
